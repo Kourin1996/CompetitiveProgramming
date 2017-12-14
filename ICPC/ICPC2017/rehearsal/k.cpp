5a1a61c3085a83d323460f0cd330abe56bb54cfb
kourin <kourin1996@gmail.com>
2017-12-14 15:36:36 +0900

    rm ICPC

--- ICPC/ICPC2017/rehearsal/k.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <iomanip>
-
-int main(void){
-    long a, b;
-    while(std::cin >> a >> b, !(a == 0 && b == 0) ){
-        std::cout << std::fixed << std::setprecision(10) << (long double)a/b << std::endl;
-    }
-}
